a6bfc70d57dac49e1ca69497267b253447d00b97
ChauDing <1603661@rgu.ac.uk>
2018-04-11 21:41:37 +0100

    twittter in

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'vfmsSeh2Zt3mmseF01IvNv9Kz',
+  consumer_secret:      'PomCEGZymNZ4wr77Cc1xsxlxvQFDYkRojcAeuicig5PHkY96vY',
+  access_token:         '963135842532384772-ISRbrCS2RzQNt6WyHyFIpgJq8UQmcAL',
+  access_token_secret:  'hT6e4U7mezuwOilhAME8zRPZV8CrfLdTFzDpcGL8oprjB'
+}
--- twit.js
+++ twit.js
@@ -1,17 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var ejs = require('ejs');
+console.log('Twitter is on');
+
 var twit = require('twit');
-var config = require ('../config.js');
+
+var config = require ('./config.js');
 var T = new twit(config);
 var params = {
     q: '#rainbow',
     count: 50
-};
-router.get('/', function (req, res) {
-  res.render('pages/index');
-});
-
+}
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response) {
@@ -22,14 +18,5 @@ function gotData(err, data, response) {
      console.log('created by @'+tweets[i].user.screen_name);
      console.log(tweets[i].text);
     }
+   // console.log(data.statuses);
 };
-
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/login');
-	};
-};
-module.exports = router;
