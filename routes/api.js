ad0a3569b37c1c5a2b4e049daae4e0477360be17
Christine Chau <1603661@rgu.ac.uk>
2018-04-27 10:22:29 +0000

    commented out the instagram token keys

--- routes/api.js
+++ routes/api.js
@@ -10,10 +10,10 @@ router.use( bodyParser.json() );
 router.use(express.urlencoded());
 var config = require ('../config.js');
 var twitterAPI = new twit(config);
-
-ig.use({ access_token: '12930071.4771b68.7c3097a46b484a63aaf31632aca9cb65' });
-ig.use({ client_id: 'cdba839f56bc4d17a663a5e94d6ef5a4',
-         client_secret: '625b44ef32824f25882c89c56880d965' });
+// was going to add the instagram getter method in here butu due to authentication restrictions we were unable to complete this
+// ig.use({ access_token: '12930071.4771b68.7c3097a46b484a63aaf31632aca9cb65' });
+// ig.use({ client_id: 'cdba839f56bc4d17a663a5e94d6ef5a4',
+//          client_secret: '625b44ef32824f25882c89c56880d965' });
 //get based on search term, count, location, etc
 router.post('/', function (req, res) {
     var search = req.body.search;
